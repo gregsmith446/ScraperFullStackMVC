cdcfc37039a30ec4d0080a3376176be48d7c083f
Greg Smith <gregsmith446@gmail.com>
2019-06-18 20:31:04 -0400

    updated views csHTML

--- ScraperFullStackMVC/Controllers/HomeController.cs
+++ ScraperFullStackMVC/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace ScraperFullStackMVC.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
